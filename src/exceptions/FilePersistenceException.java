3b5ba78d18a4c19ecc0657f3732bcfbc338814c5
FerAlvarez25 <luisa.alvarez05@uptc.edu.co>
2024-10-16 18:22:42 -0500

    excepciones y vista

---
+++ src/exceptions/FilePersistenceException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+
+    public class FilePersistenceException extends Exception {
+        public FilePersistenceException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+    
+
